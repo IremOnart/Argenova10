7ae9c34f07a8a04557de18139c148f1ccc33c3aa
IremOnart <78277981+IremOnart@users.noreply.github.com>
2021-06-25 13:34:35 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,92 @@
+
+    <footer class="footer">
+        <div class="footer-left">
+            <img src="braintech-removebg-preview.png" alt="">
+            <p>
+                Sedut perspiciatis unde omnis iste natus error sitlutem acc usantium doloremque denounce with illo
+                inventore veritatis
+            </p>
+            <div class="socials">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-pinterest"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+
+            </div>
+        </div>
+        <ul class="footer-right">
+            <li>
+                <h2>IT Services</h2>
+
+                <ul class="box">
+                    <li><a href="#">Software Development</a></li>
+                    <li><a href="#">Web Development</a></li>
+                    <li><a href="#">Analytic Dolutions</a></li>
+                    <li><a href="#">Cloud and DevOps</a></li>
+                    <li><a href="#">Project Design</a></li>
+
+                </ul>
+
+            </li>
+            <li class="features">
+                <h2>Contact Info</h2>
+
+                <ul class="box">
+                    <li><a href="#">374 FA Tower, William S Blvd 2721, IL, USA</a></li>
+                    <li><a href="#">(+880)155-69569</a></li>
+                    <li><a href="#">support@gmail.com</a></li>
+                    <li><a href="#">Opening Hours: 10:00-18:00</a></li>
+                </ul>
+
+            </li>
+            <li>
+                <h2>Newsletter</h2>
+                <p>
+                    We denounce with righteous and in and dislike men who are so beguiled and demo realized.
+                </p>
+                
+
+            </li>
+
+        </ul>
+
+    </footer>
+    <div class="container3">
+        <div class="footer-bottom-left">
+            <p>
+                © 2021 All Rights Reserved. Developed By RSTheme
+            </p>
+        </div>
+        <div class="footer-bottom-right">
+            <ul class="footer-bottomlast">
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Home</a></li>
+
+
+
+
+            </ul>
+
+        </div>
+
+
+
+
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+        integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <title>Braintech</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="ornek">
+            <img class="logo" src="braintech-removebg-preview.png" alt="logo">
+            <p class="yazı" id="yazi1"> <strong> Address E-mail Phone </strong></p>
+            <p class="yazı" id="yazi2">05kandiBR.NewYork support@gmail.com +019988772</p>
+
+    </header>
+
+    <nav>
+        <div class="container">
+            <div class="left-side">
+                <ul class="menu">
+                    <li><a href="#">Home</a>
+                    <ul>
+                        <li><a href="#">Multipages</a>
+                        <ul>
+                            <li><a href="#">Main Demo</a></li>
+                            <li><a href="#">Digital Agency 01</a></li>
+                            <li><a href="#">IT Solution 01</a></li>
+                            <li><a href="#">Digital Agency 02</a></li>
+                            <li><a href="#">Software Solution</a></li>
+                            <li><a href="#">Data Analysis</a></li>
+                            <li><a href="#">IT solution 02</a></li>
+                            <li><a href="#">Gadgets Repairs</a></li>
+                            <li><a href="#">Application Testing</a></li>
+                            <li><a href="#">IT Solution 03</a></li>
+                            <li><a href="#">Digital Solution Dark</a></li>
+                            <li><a href="#">Web Design Agency</a></li>
+                            <li><a href="#">Branding Agency</a></li>
+                           
+                        </ul>
+                        </li>
+                        <li><a href="#">Onepages</a>
+                            <ul>
+                                <li><a href="#">Main Demo</a></li>
+                                <li><a href="#">Digital Agency 01</a></li>
+                                <li><a href="#">IT Solution 01</a></li>
+                                <li><a href="#">Digital Agency 02</a></li>
+                                <li><a href="#">Software Solution</a></li>
+                                <li><a href="#">Data Analysis</a></li>
+                                <li><a href="#">IT solution 02</a></li>
+                                <li><a href="#">Gadgets Repairs</a></li>
+                                <li><a href="#">Application Testing</a></li>
+                                <li><a href="#">IT Solution 03</a></li>
+                                <li><a href="#">Digital Solution Dark</a></li>
+                                <li><a href="#">Web Design Agency</a></li>
+                                <li><a href="#">Branding Agency</a></li>
+                               
+                            </ul>
+                        </li>
+                    </ul>
+                    </li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a>
+                        <ul>
+                            <li><a href="#">Software Development</a></li>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">Analytic Solutions</a></li>
+                            <li><a href="#">Cloud And DevOps</a></li>
+                            <li><a href="#">Project Design</a></li>
+                            <li><a href="#">Data Center</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Pages</a>
+                    <ul>
+                        <li><a href="#">Services</a>
+                        <ul>
+                            <li><a href="#">Services 1</a></li>
+                            <li><a href="#">Services 2</a></li>
+                            <li><a href="#">Services 3</a></li>
+                        </ul>
+                        </li>
+                        <li><a href="#">Our Team</a></li>
+                        <li><a href="#">Single Team</a></li>
+                        <li><a href="#">Case Studies</a>
+                        <ul>
+                            <li><a href="#">Case Studies Style 1</a></li>
+                            <li><a href="#">Case Studies Style 2</a></li>
+                            <li><a href="#">Case Studies Style 3</a></li>
+                            <li><a href="#">Case Studies Style 4</a></li>
+                            <li><a href="#">Case Studies Style 5</a></li>
+                            <li><a href="#">Case Studies Style 6</a></li>
+                            <li><a href="#">Case Studies Style 7</a></li>
+                        </ul>
+                        </li>
+                        <li><a href="#">Shop</a>
+                        <ul>
+                            <li><a href="#">Shop</a></li>
+                            <li><a href="#">Shop Single</a></li>
+                            <li><a href="#">Cart</a></li>
+                            <li><a href="#">Checkout</a></li>
+                            <li><a href="#">My Account</a></li>
+                        </ul>
+                        </li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Faq</a></li>
+                    </ul>
+                </li>
+                    <li><a href="#">Blog</a>
+                    <ul>
+                        <li><a href="http://127.0.0.1:5500/blog/index2.html">Blog</a></li>
+                        <li><a href="http://127.0.0.1:5500/blog%20detay%20sayfas%C4%B1/index.html">Blog Details</a></li>
+                    </ul>
+                    </li>
+                    <li><a href="http://127.0.0.1:5500/ileti%C5%9Fim%20formu/index.html">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="right-side">
+                <ul class="socialmedia">
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-pinterest"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-facebook-f"></a>
+                </ul>
+
+            </div>
+
+        </div>
+    </nav>
+    
