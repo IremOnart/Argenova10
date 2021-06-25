b61bf5004d0e39da506026886a1dd630dfd95ae4
IremOnart <78277981+IremOnart@users.noreply.github.com>
2021-06-25 13:35:53 +0300

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,214 @@
+<?php 
+    include "includes/header.php";
+?>
+
+    <img class="resim" src="servo.jpg" alt="resim">
+    <br><br><br><br><br><br>
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12 col-md-3">
+                <div class="kutu">
+                    <a href="#"><img class="img" src="5.jpg" alt=""></a> <br><br>
+                    <p>
+                        January 21,2020     admin     Strategy
+                    </p>
+                
+                    <p>
+                        We denounce with righteous indige nation and dislike men who are so beguiled and demo realized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue cannot foresee. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled data structures manages data in technology.
+                    </p><br><br>
+                    <div class="kutu2">
+                        We can easily manage if we will only take, each day, the burden appointed to it. But the load will be too heavy for us if we carry yesterday’s burden over again today, and then add the burden of the morrow before we are required to bear it.
+                        <br><br>
+                        <p>
+                            <strong>— Robert Calibo</strong> 
+                        </p>
+                    </div><br><br><br>
+                    <h2 class="head">
+                        Digital technology on the cutting edge
+                    </h2>
+                    <br>
+                    <ul>
+                        <li>How will digital activities impact traditional manufacturing.</li>
+                        <li>All these digital elements and projects aim to enhance .</li>
+                        <li>I monitor my staff with software that takes screenshots.</li>
+                        <li>Laoreet dolore magna niacin sodium glutimate aliquam hendrerit.</li>
+                        <li>Minim veniam quis niacin sodium glutimate nostrud exerci dolor.</li>
+                    </ul>
+                    <br>
+                    <p>
+                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+                    </p>
+                    <img class="img" src="6.jpg" alt=""><br><br>
+                    <p>
+                        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.
+                    </p><br>
+                    <h2 class="head">
+                        1 comment on “Servo Project Joins The Linux Foundation Desco”
+                    </h2>
+                    <div class="kutu3">
+                       <strong>Admin</strong><br>
+                       December 3, 2020 at 8:30 am
+                       <br>
+                       Aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est
+                       <br><br>
+                       <button type="submit" class="btn btn-primary">Reply</button>
+                    
+                    </div><hr><br>
+                    
+                    <h2>
+                        Leave a Reply
+                    </h2><br>
+                    Your email address will not be published. Required fields are marked *
+                    <br><br>
+                    <div class="row g-3">
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="Name*" aria-label="Name">
+                        </div>
+                        <div class="col">
+                            <input type="text" class="form-control" placeholder="E-Mail*" aria-label="E-Mail">
+                        </div>
+                    </div>
+    
+                    <br>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                    placeholder="Your Message Here"></textarea>
+
+
+                <br>
+                <button type="submit" class="butun-last">Post Comment</button>
+
+
+
+
+                    
+                </div>
+
+            </div>
+
+
+
+
+            <div class="col-lg-6 col-sm-12 col-md-3">
+
+                <div class="ilk">
+                    <form class="example" action="action_page.php">
+                        <input type="text" placeholder="Search.." name="search">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+
+                </div>
+                <div class="ikinci">
+                    <br><br>
+                    <h2>
+                        Lastest Post
+                    </h2>
+                    <br>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <a href="#"><img src="1.jpg" alt="" width="100"></a>
+
+
+
+                        </div>
+                        <div class="col-lg-6">
+                            <p class="ilkyazi">
+                                <a href="#" class="dd"> Pen Source Job Report Show More Openings Fewer</a>
+
+                            </p>
+
+                        </div>
+
+                    </div><br>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <a href="#"><img src="2.jpg" alt="" width="100"></a>
+
+
+
+                        </div>
+                        <div class="col-lg-6">
+                            <a href="#" class="dd">Tech Products That Makes Its Easier to Stay at Home</a>
+
+
+                        </div>
+
+                    </div><br><br>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <a href="#"><img src="3.jpg" alt="" width="100"></a>
+
+
+
+                        </div>
+                        <div class="col-lg-6">
+
+                            <a href="#" class="dd">Necessity May Give Us Your Best Virtual Court System</a>
+
+                        </div>
+
+                    </div> <br><br>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <a href="#"><img src="4.jpg" alt="" width="100"></a>
+
+
+
+                        </div>
+                        <div class="col-lg-6">
+
+                            <a href="#" class="dd">Servo Project Joins The Linux Foundation Fold Desco</a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="ucuncu">
+                    <br>
+                    <h2>
+                        Categories
+                    </h2><br>
+                    <p>
+                        <a href="#" class="dd">Artifical Intelligence</a>
+
+                    </p><br>
+                    <p>
+                        <a href="#" class="dd">Digital Technology</a>
+
+                    </p><br>
+                    <p>
+                        <a href="#" class="dd">IT Services</a>
+
+                    </p><br>
+                    <p>
+                        <a href="#" class="dd">Software Development</a>
+
+                    </p><br>
+                    <p>
+                        <a href="#" class="dd"> Web Development</a>
+
+                    </p>
+
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+
+    <br>
+
+
+<?php 
+    include "includes/footer.php";
+?>
+ 
